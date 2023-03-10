bf1b3955a4b9c76c4d3910dc8a79559846cfd93d
Andrew Shlegel <a.shlegel@outlook.com>
2023-03-10 14:39:02 -0300

    Task034 added

---
+++ Task034/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+void PrintArray(int[] array)
+{
+    Console.Write($"[{String.Join(", ", array)}]");
+}
+
+int[] CreateArray(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(100, 1000);
+    }
+    return array;
+}
+
+int EvenCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] myArray = CreateArray(10);
+PrintArray(myArray);
+Console.Write($" -> {EvenCount(myArray)}");
+Console.WriteLine();
